c1fe8890a342c0fe55c22d9c3739f11b81f014c6
Kaya <kayalion@kayalion.net>
2014-12-30 17:06:56 +0100

    added flag to cast invalid dates to null

--- src/ride/library/decorator/DateFromFormatDecorator.php
+++ src/ride/library/decorator/DateFromFormatDecorator.php
@@ -8,6 +8,21 @@ namespace ride\library\decorator;
 class DateFromFormatDecorator extends DateFormatDecorator {
 
     /**
+     * Flag to see if null should be returned for a invalid date
+     * @var boolean
+     */
+    protected $invalidToNull;
+
+    /**
+     * Sets the flag to see if null should be returned for a invalid date
+     * @param boolean $invalidToNull
+     * @return null
+     */
+    public function setInvalidToNull($invalidToNull) {
+        $this->invalidToNull = $invalidToNull;
+    }
+
+    /**
      * Decorates a formatted date into a UNIX timestamp
      * @param mixed $value
      * @return mixed A UNIX timestamp if a valid formatted date has been
@@ -26,6 +41,10 @@ class DateFromFormatDecorator extends DateFormatDecorator {
 
         $date = date_create_from_format($dateFormat, $value);
         if ($date === false) {
+            if ($this->invalidToNull) {
+                return null;
+            }
+
             return $value;
         }
 
